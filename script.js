256d0e7f1ade433045338931656aebca64ce7225
hjali7 <88620768+hjali7@users.noreply.github.com>
2022-08-10 20:21:25 +0430

    finished swiper slider

---
+++ script.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    certainSlide : true,
+    fade: true,
+    gragCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        520:{
+            slidesPerView: 2,
+        },
+        960:{
+            slidesPerView: 3,
+        },
+    },
+  });
